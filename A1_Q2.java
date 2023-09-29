cab3a36c43442ca4506c5eb86da0daa6de8ec119
Joel-Saxena <145243393+Joel-Saxena@users.noreply.github.com>
2023-09-29 18:19:35 +0530

    Update A1_Q2.java

--- A1_Q2.java
+++ A1_Q2.java
@@ -1,19 +1,26 @@
 import java.util.*;
-public class A_1_10
+public class gradsys
 {
     public static void main()
     {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the number");
-        int n=sc.nextInt();
-        System.out.println("The prime factors are:");
-        for(int i=2;i<=n;i++)
-        {
-            while(n%i==0)
-            {
-                System.out.print(i+" ");
-                n=n/i;
-            }
-        }
+        Scanner sc= new Scanner(System.in);
+        /*System.out.println("Enter maximum marks");
+        double mm= sc.nextDouble();*/
+        System.out.println("Enter students's marks");
+        double m= sc.nextDouble();
+        //double p=m/mm*100.0;
+        if(m>90 && m<=100)
+        System.out.println("Excellent");
+        else if(m>80 && m<=90)
+        System.out.println("Good");
+        else if(m>70 && m<=80)
+        System.out.println("Fair");
+        else if(m>60 && m<=70)
+        System.out.println("Satisfactory");
+        else if(m<=60 && m>=0)
+        System.out.println("Below Par");
+        else
+        System.out.println("Invalid Input");
     }
+    
 }
